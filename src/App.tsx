cbec22c36aaf690980b81f6c2093904d72feaa23
Ottih Chuks <chukwukaottih@gmail.com>
2026-04-16 11:53:26 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ const App = () => {
   return (
     <AnimatePresence>
       {showLoader && (
-        <LoadingScreen onComplete={() => setShowLoader(false)} minDuration={1200} />
+        <LoadingScreen onComplete={() => setShowLoader(false)} minDuration={100} />
       )}
 
       <QueryClientProvider client={queryClient} key="app">
